0e01a8c7098d01c14fffaf9b9509eb0912f3807a
JordanDavidAx <jdavid@axome.com>
2024-08-30 18:37:31 +0200

    :sparkles: Style page finish

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,1 +1,7 @@
 console.log('Coucou')
+
+document.querySelectorAll('.dropdown-options_item').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
